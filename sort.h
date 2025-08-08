d33a920c6a8989de2856e75043f11e295196b078
Sedra Ramarosaona <sedranante@gmail.com>
2025-08-08 12:33:15 +0200

    function that sorts an array in the quick sorting mode

--- sort.h
+++ sort.h
@@ -30,5 +30,7 @@ int *rand_array(int len, int max);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quicksort(int *array, size_t size, ssize_t lo, ssize_t hi);
+void quick_sort(int *array, size_t size);
 
 #endif
